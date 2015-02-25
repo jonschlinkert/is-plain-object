48f326d18bcf8776c60cdf7c7859d2dc514e6257
jonschlinkert <jon.schlinkert@sellside.com>
2015-02-25 02:49:24 -0500

    adds [isobject](https://github.com/jonschlinkert/isobject), tests

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 /*!
  * is-plain-object <https://github.com/jonschlinkert/is-plain-object>
  *
- * Copyright (c) 2014 Jon Schlinkert, contributors.
- * Licensed under the MIT License
+ * Copyright (c) 2014-2015, Jon Schlinkert.
+ * Licensed under the MIT License.
  */
 
 'use strict';
 
+var isObject = require('isobject');
+
 module.exports = function isPlainObject(o) {
-  return !!o && typeof o === 'object' && o.constructor === Object;
+  return isObject(o) && o.constructor === Object;
 };

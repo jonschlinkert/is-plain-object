315a235114739b4f3a2df73477d481c33d40851f
jonschlinkert <jon.schlinkert@sellside.com>
2017-05-27 15:51:46 -0400

    run update, update deps

--- test/server.js
+++ test/server.js
@@ -1,34 +1,34 @@
 /*!
  * is-plain-object <https://github.com/jonschlinkert/is-plain-object>
  *
- * Copyright (c) 2014-2015, Jon Schlinkert.
+ * Copyright (c) 2014-2017, Jon Schlinkert.
  * Licensed under the MIT License.
  */
 
 'use strict';
 
-/* deps:mocha */
-var expect = require('chai').expect;
-var isPlainObject = require('../');
+require('mocha');
+var assert = require('assert');
+var isPlainObject = require('..');
 
 describe('Same-Realm Server Tests', function() {
   it('should return `true` if the object is created by the `Object` constructor.', function() {
-    expect( isPlainObject(Object.create({})) ).to.be.true;
-    expect( isPlainObject(Object.create(Object.prototype)) ).to.be.true;
-    expect( isPlainObject({foo: 'bar'}) ).to.be.true;
-    expect( isPlainObject({}) ).to.be.true;
+    assert(isPlainObject(Object.create({})));
+    assert(isPlainObject(Object.create(Object.prototype)));
+    assert(isPlainObject({foo: 'bar'}));
+    assert(isPlainObject({}));
   });
 
   it('should return `false` if the object is not created by the `Object` constructor.', function() {
     function Foo() {this.abc = {};};
 
-    expect( isPlainObject(/foo/) ).to.be.false;
-    expect( isPlainObject(function () {}) ).to.be.false;
-    expect( isPlainObject(1) ).to.be.false;
-    expect( isPlainObject(['foo', 'bar']) ).to.be.false;
-    expect( isPlainObject([]) ).to.be.false;
-    expect( isPlainObject(new Foo) ).to.be.false;
-    expect( isPlainObject(null) ).to.be.false;
-    expect( isPlainObject(Object.create(null)) ).to.be.false;
+    assert(!isPlainObject(/foo/));
+    assert(!isPlainObject(function() {}));
+    assert(!isPlainObject(1));
+    assert(!isPlainObject(['foo', 'bar']));
+    assert(!isPlainObject([]));
+    assert(!isPlainObject(new Foo));
+    assert(!isPlainObject(null));
+    assert(!isPlainObject(Object.create(null)));
   });
 });

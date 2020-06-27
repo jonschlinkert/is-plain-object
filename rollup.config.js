e9c7b3e64049f40c5f7d0abff033caec75bec7df
Bogdan Chadkin <trysound@yandex.ru>
2020-06-27 11:06:10 +0300

    Bundle isobject to make the package dependency free

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,23 @@
-import nodeResolve from 'rollup-plugin-node-resolve';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 
-export default [
-  {
-    input: './index.js',
-    output: {
+export default {
+  input: './index.js',
+  output: [
+    {
       format: 'iife',
       file: 'browser/is-plain-object.js',
       name: 'isPlainObject'
     },
-    plugins: [
-      nodeResolve(),
-    ]
-  },
-
-  {
-    input: './index.js',
-    output: {
+    {
       format: 'cjs',
       file: 'index.cjs.js'
     },
-    plugins: [
-      nodeResolve()
-    ]
-  }
-]
+    {
+      format: 'es',
+      file: 'index.es.js'
+    }
+  ],
+  plugins: [
+    nodeResolve(),
+  ]
+}

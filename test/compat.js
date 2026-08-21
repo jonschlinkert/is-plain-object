1ac078d5bb4d8c5291a79863c98e8c8468a7bce0
Bogdan Chadkin <opensource@trysound.io>
2026-08-21 11:38:42 +0200

    Run Node tests without the legacy ESM loader

--- test/compat.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var assert = require('assert');
-var isPlainObject = require('../dist/is-plain-object.js').isPlainObject;
-
-assert(isPlainObject(Object.create(null)));
-assert(isPlainObject({ foo: 'bar' }));
-assert(!isPlainObject([]));
-assert(!isPlainObject(null));

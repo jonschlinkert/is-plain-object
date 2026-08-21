7660dc8e1055dfd71464dcaf66158a0857226713
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2026-08-21 22:12:45 +1200

    feat: use a type guard for the function (#29)

--- is-plain-object.d.ts
+++ is-plain-object.d.ts
@@ -1,1 +1,6 @@
-export function isPlainObject(o: any): boolean;
+/**
+ * Returns true if an object was created by the `Object` constructor, or Object.create(null).
+ * 
+ * @param value The value to test
+ */
+export function isPlainObject(value: unknown): value is Record<PropertyKey, unknown>;
